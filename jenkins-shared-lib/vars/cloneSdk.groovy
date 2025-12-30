cec61cd65b266fc0fbc961377e0f2f98145de060
manjunath-grl <manjunath@graniteriverlabs.in>
2025-12-30 22:51:00 +0530

    fix

--- jenkins-shared-lib/vars/cloneSdk.groovy
+++ jenkins-shared-lib/vars/cloneSdk.groovy
@@ -1,9 +1,15 @@
+// vars/cloneSdk.groovy
 def call(sdkCfg, dirName) {
     dir(dirName) {
         checkout([
             $class: 'GitSCM',
             branches: [[name: sdkCfg.branch]],
-            userRemoteConfigs: [[url: sdkCfg.repoUrl]]
+            userRemoteConfigs: [[url: sdkCfg.repoUrl]],
+            extensions: sdkCfg.submodules ? [
+                [$class: 'SubmoduleOption',
+                 recursiveSubmodules: true,
+                 trackingSubmodules: false]
+            ] : []
         ])
     }
 }
