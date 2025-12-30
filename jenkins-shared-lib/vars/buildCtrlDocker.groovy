d637564f6e472a13907f315e3ebade2e0b3c9f68
manjunath-grl <manjunath@graniteriverlabs.in>
2025-12-30 20:33:35 +0530

    Docker build func update

--- jenkins-shared-lib/vars/buildCtrlDocker.groovy
+++ jenkins-shared-lib/vars/buildCtrlDocker.groovy
@@ -1,3 +1,4 @@
+// vars/buildCtrlDocker.groovy
 def call(ciConfig) {
 
     def dockerImage = ciConfig.ci_config.build_config.ctrl.docker_image ?: "chip_image:latest"
@@ -12,26 +13,28 @@ def call(ciConfig) {
         echo "Docker Platform     : ${dockerPlatform}"
         echo "Workspace           : ${workSpace}"
 
-        def dockerCmd = """#!/bin/bash
+        sh """#!/bin/bash
+        set -ex
+
+        docker run --rm --user root --platform=${dockerPlatform} \
+          -v "${workSpace}/ctrl_sdk:/home/connectedhome" \
+          -w /home/connectedhome \
+          ${dockerImage} \
+          /bin/bash -c "
             set -ex
+            git config --global --add safe.directory /home/connectedhome
+            git config --global --add safe.directory /home/connectedhome/third_party/pigweed/repo
 
-            docker run --rm --user root --platform=${dockerPlatform} -v ${workSpace}/ctrl_sdk:/home/connectedhome \\
-            -w /home/connectedhome ${dockerImage}:latest \\
-            /bin/bash -c \'
-                set -ex  # Stop execution on first error
-                git config --global --add safe.directory /home/connectedhome
-                git config --global --add safe.directory /home/connectedhome/third_party/pigweed/repo
-                git config --global http.version HTTP/1.1
-                git config --global http.postBuffer 524288000
-                git config --global http.lowSpeedLimit 0
-                git config --global http.lowSpeedTime 999999
-                ./scripts/checkout_submodules.py --allow-changing-global-git-config --shallow --platform linux
-                source scripts/bootstrap.sh
-                source scripts/activate.sh
-                ./scripts/examples/gn_build_example.sh examples/chip-tool out/chip-tool chip_mdns="platform" chip_inet_config_enable_ipv4=false
-              \'
+            ./scripts/checkout_submodules.py --allow-changing-global-git-config --shallow --platform linux
+            source scripts/bootstrap.sh
+            source scripts/activate.sh
+
+            ./scripts/examples/gn_build_example.sh \
+              examples/chip-tool \
+              out/chip-tool \
+              chip_mdns=platform \
+              chip_inet_config_enable_ipv4=false
+          "
         """
-
-        sh dockerCmd
     }
 }
--- jenkins-shared-lib/vars/buildDutDocker.groovy
+++ jenkins-shared-lib/vars/buildDutDocker.groovy
@@ -1,34 +1,57 @@
-// vars/buildCtrlDocker.groovy
+// vars/buildDutDocker.groovy
 def call(ciConfig) {
 
-    def ctrlCfg = ciConfig.ci_config.build_config.ctrl
-    def dockerImage = ctrlCfg.docker_image ?: error("CTRL docker_image missing")
+    def dutCfg = ciConfig.ci_config.build_config.dut
+    if (!dutCfg?.docker_image) {
+        error "DUT docker_image missing in ci_config.build_config.dut"
+    }
 
-    stage('Build CTRL in Docker') {
+    def dockerImage = dutCfg.docker_image
+    def workSpace   = pwd()
+
+    stage('Build DUT in Docker') {
 
         def arch = sh(script: "uname -m", returnStdout: true).trim()
         def dockerPlatform = (arch == "x86_64") ? "linux/amd64" : "linux/arm64"
 
+        echo "DUT Docker Image : ${dockerImage}"
+        echo "Docker Platform  : ${dockerPlatform}"
+        echo "Workspace        : ${workSpace}"
+
         sh """#!/bin/bash
-            set -ex
-            docker run --rm --user root --platform=${dockerPlatform} -v ${workSpace}/ctrl_sdk:/home/connectedhome \\
-            -w /home/connectedhome ${dockerImage}:latest \\
-            /bin/bash -c \"
-                set -ex  # Stop execution on first error
+            set -euo pipefail
+
+            docker run --rm --user root --platform=${dockerPlatform} \\
+              -v ${workSpace}/dut_sdk:/home/connectedhome \\
+              -w /home/connectedhome \\
+              ${dockerImage} \\
+              /bin/bash -c '
+                set -ex
+
                 git config --global --add safe.directory /home/connectedhome
                 git config --global --add safe.directory /home/connectedhome/third_party/pigweed/repo
+
                 git config --global http.version HTTP/1.1
                 git config --global http.postBuffer 524288000
                 git config --global http.lowSpeedLimit 0
                 git config --global http.lowSpeedTime 999999
-                ./scripts/checkout_submodules.py --allow-changing-global-git-config --shallow --platform linux
+
+                ./scripts/checkout_submodules.py \\
+                    --allow-changing-global-git-config \\
+                    --shallow \\
+                    --platform linux
+
                 source scripts/bootstrap.sh
                 pw cli-analytics --opt-out
                 source scripts/activate.sh
-                # TODO: -n false is a temporary workaround needs to be updated it to dynamic bases on the configuration.
-                scripts/build_python.sh -m platform -d true -i out/python_env -n false
-                ./scripts/examples/gn_build_example.sh examples/all-clusters-app/linux out/all-clusters-app chip_inet_config_enable_ipv4=false chip_device_config_enable_wifipaf=true
-            \"
+
+                # DUT APP BUILD (example: all-clusters-app)
+                ./scripts/examples/gn_build_example.sh \\
+                  examples/all-clusters-app/linux \\
+                  out/all-clusters-app \\
+                  chip_inet_config_enable_ipv4=false \\
+                  chip_device_config_enable_wifipaf=true
+              '
         """
     }
 }
