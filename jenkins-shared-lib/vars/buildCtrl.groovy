144d625091ec8e665ddcd274d3d4a09e87481edb
manjunath-grl <manjunath@graniteriverlabs.in>
2025-12-30 23:42:55 +0530

    cd ${jobName}/ctrl_sdk

--- jenkins-shared-lib/vars/buildCtrl.groovy
+++ jenkins-shared-lib/vars/buildCtrl.groovy
@@ -2,16 +2,17 @@ def call(ciConfig) {
 
     def ctrlCfg = ciConfig.ci_config.build_config.ctrl
     def workSpace = pwd()
+    def jobName = env.JOB_NAME.replaceAll('/', '_')
 
     stage('Build CTRL (Docker)') {
 
         def arch = sh(script: "uname -m", returnStdout: true).trim()
         def dockerPlatform = (arch == "x86_64") ? "linux/amd64" : "linux/arm64"
 
-        echo "Workspace         : ${workSpace}"
+        echo "Workspace : ${workSpace}"
 
         sh """
-        cd ctrl_sdk
+        cd ${jobName}/ctrl_sdk
         ./scripts/pi-setup/auto-install.sh
         """
     }
