74b601ea75e74974843bc6aa94946eb366390fed
manjunath-grl <manjunath@graniteriverlabs.in>
2025-12-31 13:31:23 +0530

    debug statements

--- jenkins-shared-lib/vars/copyArtifactsEx.groovy
+++ jenkins-shared-lib/vars/copyArtifactsEx.groovy
@@ -1,7 +1,9 @@
 def call(type, cfg) {
+    echo "Copying artifacts for ${type}"
     def cfgBlock = cfg.ci_config.copy_build_artifact
     def projectName = env.JOB_NAME
     def selector = env.BUILD_NUMBER
+    echo "Copying artifacts for ${type} from job: ${projectName}, build: ${selector}"
     if (cfgBlock.${type}.enabled) {
         projectName = cfgBlock.${type}.job_name
         selector = cfgBlock.${type}.build_number
